79a51da036b860b8a73cb35569082c0f1ade17c0
Đinh Văn Hữu Nghĩa <86728246+Juil53@users.noreply.github.com>
2021-11-13 11:20:22 +0700

    update lai link mockapi

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,28 +1,120 @@
 var service = new ProductSerive();
-function getListProduct(){
+function getListProduct() {
     service
-    .getListProductAPI()
-    .then(function(result){
-        console.log(result);
-        renderData(result.data);
-    })
-    .catch(function(error){
-        console.log(error);
-    });
+        .getListProductAPI()
+        .then(function (result) {
+            renderData(result.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 }
 getListProduct();
 
-function renderData(data){
-    var html = "";
-    data.forEach(function(item){
-        var trSP = `<tr>
-        <td>${item.id}</td>
+function renderData(data) {
+    var contenthtml = "";
+    data.forEach(function (item, index) {
+        var trSP =
+            `<tr>
+        <td>${index + 1}</td>
         <td>${item.tenSP}</td>
         <td>${item.gia}</td>
-        <td><img style="width:10%"; src="../../assets/img/${item.hinhAnh}" alt=""></td>
+        <td><img style="width:50px"; src="../../assets/img/${item.hinhAnh}" alt=""></td>
         <td>${item.moTa}</td>
+        <td>
+            <button class="btnSuaSP btn btn-info" data-toggle="modal" data-target="#myModal" onclick="editProduct(${item.id})">Edit</button>
+            <button class="btn btn-danger" onclick="deleteProduct(${item.id})">Delete</button>
+        </td>
         </tr>`;
-        html += trSP;
+        contenthtml += trSP;
     });
-    document.getElementById("tblDanhSachSP").innerHTML = html;
+    document.getElementById("tblDanhSachSP").innerHTML = contenthtml;
+}
+
+/**
+ * Delete product
+ */
+function deleteProduct(id) {
+    console.log(id);
+    service.deleteProductAPI(id)
+        .then(function () {
+            alert("Delete sucess!");
+            //Làm mới dữ liệu mới nhất từ server
+            getListProduct();
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
+
+document.getElementById("btnThemSP").addEventListener("click", function () {
+    document.getElementsByClassName("modal-title")[0].innerHTML = "Add Product";
+    var footerModal = `<button class="btn btn-success" onclick="addProduct()">Add Product</button>`;
+    document.getElementsByClassName("modal-footer")[0].innerHTML = footerModal;
+})
+/**
+ * Add Product
+ * 
+ */
+function addProduct() {
+    //Lấy value từ các thẻ input
+    var tensp = document.getElementById("TenSP").value;
+    var giasp = document.getElementById("GiaSP").value;
+    var hinhsp = document.getElementById("HinhSP").value;
+    var motasp = document.getElementById("moTa").value;
+    var product = new Product("", tensp, giasp, hinhsp, motasp);
+    console.log(product);
+    service.addProductAPI(product)
+        .then(function (result) {
+            //Tắt modal
+            document.getElementsByClassName("close")[0].click();
+            getListProduct();
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+
+/**
+ * Edit Product
+ */
+
+function editProduct(id) {
+    document.getElementsByClassName("modal-title")[0].innerHTML = "Edit Product";
+    var footerModal = `<button class="btn btn-primary" onclick="updateProduct(${id})">Update Product</button>`;
+    document.getElementsByClassName("modal-footer")[0].innerHTML = footerModal;
+
+    service
+        .getProductById(id)
+        .then(function (result) {
+            //show thông tin ra UI
+            console.log(result.data);
+            document.getElementById("TenSP").value = result.data.tenSP;
+            document.getElementById("GiaSP").value = result.data.gia;
+            document.getElementById("HinhSP").value = result.data.hinhAnh;
+            document.getElementById("moTa").value = result.data.moTa;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+}
+/**
+ * Update Product
+ */
+function updateProduct(id) {
+    var tensp = document.getElementById("TenSP").value;
+    var giasp = document.getElementById("GiaSP").value;
+    var hinhsp = document.getElementById("HinhSP").value;
+    var motasp = document.getElementById("moTa").value;
+    var product = new Product(id, tensp, giasp, hinhsp, motasp);
+    service.updateProductAPI(product)
+    .then(function (result) {
+        //Tắt modal
+        document.getElementsByClassName("close")[0].click();
+        //Làm mới Data
+        getListProduct();
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
 }
--- app/services/product_services.js
+++ app/services/product_services.js
@@ -1,8 +1,37 @@
 function ProductSerive(){
     this.getListProductAPI = function(){
         return axios({
-            url: "https://6183caea91d76c00172d1b61.mockapi.io/api/products",
+            // url: "https://6183caea91d76c00172d1b61.mockapi.io/api/products",
+            url:"https://618eab9a50e24d0017ce13c9.mockapi.io/products",
             method: "GET",
+        })
+    };
+    this.deleteProductAPI = function(id){
+        return axios({
+            // url: `https://6183caea91d76c00172d1b61.mockapi.io/api/products/${id}`,
+            url:"https://618eab9a50e24d0017ce13c9.mockapi.io/products/${id}",
+            method:"DELETE",
+        })
+    };
+    this.addProductAPI = function(product){
+        return axios({
+            // url:"https://6183caea91d76c00172d1b61.mockapi.io/api/products",
+            url: "https://618eab9a50e24d0017ce13c9.mockapi.io/products",
+            method: "POST",
+            data:product,
+        })
+    };
+    this.getProductById = function(id){
+        return axios({
+            url: "https://618eab9a50e24d0017ce13c9.mockapi.io/products/${id}",
+            method:"GET",
+        })
+    };
+    this.updateProductAPI = function(product){
+        return axios({
+            url: "https://618eab9a50e24d0017ce13c9.mockapi.io/products${product.id}",
+            method: "PUT",
+            data: product,
         })
     };
 }
